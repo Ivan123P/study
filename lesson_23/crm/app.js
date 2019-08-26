10054a8fc411e2369176d7e5282c08383883b6d4
Ivan Pavlenko <ip-xv_ix_xc@i.ua>
2019-08-26 20:53:23 +0300

    crm

--- lesson_23/crm/app.js
+++ lesson_23/crm/app.js
@@ -23,11 +23,11 @@ app.use(connection(mysql, {
 
 app.get('/',                     routes.index);
 app.get('/customers',            customers.all);
-// app.get('/customers/add',        customers.add);
-// app.post('/customers/add',       customers.save);
-// app.get('/customers/edit/:id',   customers.edit);
-// app.post('/customers/edit/:id',  customers.edit_save);
-// app.get('/customers/delete/:id', customers.delete);
+app.get('/customers/add',        customers.add);
+app.post('/customers/add',       customers.save);
+app.get('/customers/edit/:id',   customers.edit);
+app.post('/customers/edit/:id',  customers.edit_save);
+app.get('/customers/delete/:id', customers.delete);
 
 app.listen(3000, () => {
   console.log('Server at http://localhost:3000');
--- lesson_23/crm/routes/customers.js
+++ lesson_23/crm/routes/customers.js
@@ -7,6 +7,60 @@ module.exports.all = (req, res) => {
   });
 };
 
-// module.exports.add = (req, res) => {
-//   res.render('add_customer', {page_title: 'Добавить пользователя в CRM систему'});
-// };
+module.exports.add = (req, res) => {
+  res.render('add_customer', {title: 'Добавить пользователя в CRM систему'});
+};
+
+module.exports.edit = (req, res) => {
+  let id = req.params.id;
+  req.getConnection( (err, connection) => {
+    connection.query('SELECT * FROM customers WHERE id - ?', [id], (err, rows) => {
+      if(err) throw new Error;
+      res.render('edit_customer', {title: 'Изменить пользователя CRM системе', data: rows});
+    });
+  });
+};
+
+module.exports.save = (req, res) => {
+  let input = req.body;
+  req.getConnection( (err, connection) => {
+    let data = {
+      name:    input.name,
+      address: input.address,
+      email:   input.email,
+      phone:   input.phone
+    };
+    connection.query('INSERT INTO customers SET ?', [data], (err, rows) => {
+      if(err) throw new Error;
+      res.redirect('/customers');
+    });
+  });
+};
+
+module.exports.edit_save = (req, res) => {
+  let input = req.body,
+      id    = req.params.id;
+
+  req.getConnection( (err, connection) => {
+    let data = {
+      name:    input.name,
+      address: input.address,
+      email:   input.email,
+      phone:   input.phone
+    };
+    connection.query('UPDATE customers SET ? WHERE id - ?', [data, id], (err, rows) => {
+      if(err) throw new Error;
+      res.redirect('/customers');
+    });
+  });
+};
+
+module.exports.delete = (req, res) => {
+  let id = req.params.id;
+  req.getConnection( (err, connection) => {
+    connection.query('DELETE FROM customers WHERE id - ?', [id], (err, rows) => {
+      if(err) throw new Error;
+      res.redirect('/customers');
+    });
+  });
+};
